9c86f4648a0e24af2680e7a0c17eb1b2402215cf
Lukas Drescher <lukasd@student.ethz.ch>
2016-12-13 19:00:42 +0100

    Removed TipsterStreamPartition

--- src/main/scala/TipsterStreamPartition.scala
+++ src/main/scala/TipsterStreamPartition.scala
@@ -4,49 +4,8 @@ import ch.ethz.dal.tinyir.processing.{TipsterParse, XMLDocument}
 import scala.collection.mutable
 import scala.math._
 
-/** TipsterSteam partition
-  *
-  * @param parentStream   stream to partition
-  * @param partitionIds   ids of documents (enumeration starts at 0 and ends at parentStream.length) belonging to the partition
-  */
-case class TipsterStreamPartition(val parentStream: TipsterStream, val partitionIds : Set[Int]) {
-  def stream: Stream[XMLDocument] = parentStream.stream.zipWithIndex
-                                      .filter { case (doc, id) => partitionIds(id) }.map(_._1)
-  def length: Int = partitionIds.size
-}
 
-object TipsterStreamPartition {
-
-  /** Creates a random partition of 0 to maxId
-    *
-    * @param fraction
-    * @param maxId
-    */
-  private def generatePartitionIds(maxId : Int, fraction: Double): Set[Int] = {
-    val partitionSize = ceil(fraction * maxId)
-    val resultPartition = new mutable.HashSet[Int]()
-    while (resultPartition.size < partitionSize) {
-      resultPartition += util.Random.nextInt(maxId)
-    }
-    resultPartition.toSet
-  }
-
-  /** Create a random fractional partition of a TipsterStream document collection
-    *
-    * @param parentStream   stream to partition
-    * @param fraction       fraction size
-    * @return               partitioned stream
-    */
-  def create(parentStream: TipsterStream, fraction : Double): TipsterStreamPartition = {
-    return new TipsterStreamPartition(parentStream,generatePartitionIds(parentStream.length, fraction))
-  }
-}
-
-
-
-
-
-/** TipsterSteam subsample (more efficient than TipsterStreamPartition)
+/** TipsterSteam subsample (for rapid prototyping)
   *
   * @param path           path to TipsterStream data set
   * @param fraction       fraction of documents of base collection to serve
